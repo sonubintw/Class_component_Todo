62870e054ae5bbfa934b3bbf35f3b1a47b588bf6
suraj chauhan <100895916+sonubintw@users.noreply.github.com>
2023-02-17 14:48:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import Todos from "./Components/Todos";
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Todos/>
+       
+      </div>
+
+    ) 
+  }
+}
+
+export default App
